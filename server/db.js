8c37e34b0ddece898f63792dbcf182fb7a9fda67
ujwal2003 <joshiujwal2003@gmail.com>
2023-08-10 14:23:27 -0500

    added environment variable for db password

--- server/db.js
+++ server/db.js
@@ -1,8 +1,9 @@
 const Pool = require("pg").Pool;
+require('dotenv').config();
 
 const pool = new Pool({
     user: "postgres",
-    password: "",
+    password: process.env.DB_PASS,
     host: "localhost",
     port: 5432,
     database: "perntodo"
